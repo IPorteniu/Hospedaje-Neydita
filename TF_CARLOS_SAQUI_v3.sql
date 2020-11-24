de4b802e5cfac9cf5e5711ae50c4b621454cb64a
IPorteniu <43084237+IPorteniu@users.noreply.github.com>
2020-11-24 02:22:02 -0500

    Update TF_CARLOS_SAQUI_v3.sql

--- TF_CARLOS_SAQUI_v3.sql
+++ TF_CARLOS_SAQUI_v3.sql
@@ -1,15 +1,16 @@
-
 CREATE VIEW VW_RN6 AS 
 SELECT datediff(day,S.DInicioEstadia ,S.DFinEstadia) AS DATEDIF,S.CReserva,M.MMonto*0.95 AS 'Descuento'
 FROM MARKETING.Reserva S ,MARKETING.Comprobante M 
 WHERE S.CComprobante=M.CComprobante 
 
 
-CREATE PROCEDURE  RN_06
+create PROCEDURE  RN_06
 @CRESERVA INT 
 as begin
 select  *
-from VW_RN06 V6
-WHERE V6DATEDIF>7
+from VW_RN6 as V6
+WHERE V6.DATEDIF > 7 and v6.CReserva = @CRESERVA
 end
+
+exec RN_06 1
 
